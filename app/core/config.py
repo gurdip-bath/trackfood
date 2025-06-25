3ddbe4798814d2322a204e5bad4c92a71ed4435c
Gurdip Bath <bathgurdip1@gmail.com>
2025-06-25 11:06:25 +0100

    setup: add config.py file with pydantic basesettings to load env vars

---
+++ app/core/config.py
@@ -0,0 +1,13 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    JWT_SECRET: str
+    JWT_ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    class Config:
+        env_file = ".env"
+
+# Export an instance to import elsewhere
+settings = Settings()

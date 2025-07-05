5d2adb67b4b12098ee802178aedf23dcc633b021
Gurdip Bath <bathgurdip1@gmail.com>
2025-07-05 17:02:25 +0100

    feat: add login file to schemas folder

--- app/services/user.py
+++ app/services/user.py
@@ -2,8 +2,12 @@ from sqlalchemy.orm import Session
 from app.schemas.user import UserCreate
 from app.models.user import User
 from app.core.security import hash_password
+from fastapi import HTTPException
 
 def create_user(db: Session, user_in: UserCreate) -> User:
+    existing_user = db.query(User).filter(User.email == user_in.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered.")
     hashed_pw = hash_password(user_in.password)
     db_user = User(email=user_in.email, hashed_password=hashed_pw)
     db.add(db_user)

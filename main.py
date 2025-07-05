390bafc1d310ab07f059b519e5668dbb847a4010
Gurdip Bath <bathgurdip1@gmail.com>
2025-07-05 13:34:56 +0100

    Feat: add router to main.py file

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from app.core.config import settings
 from app.routers import user  
+from app.core.database import Base, engine
 
 app = FastAPI()
 
 app.include_router(user.router)
+
+# Create tables if they don't exist
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def read_root():

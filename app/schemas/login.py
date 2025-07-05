24b0006d28cb25f697d40b4d5d46f0f11992feca
Gurdip Bath <bathgurdip1@gmail.com>
2025-07-05 17:14:12 +0100

    feat: define input output models for login

--- app/schemas/login.py
+++ app/schemas/login.py
@@ -1,1 +1,12 @@
 from pydantic import BaseModel
+
+# Input for login
+class LoginRequest(BaseModel):
+    email: str
+    password: str
+
+# Output for login response
+
+class LogingResponse(BaseModel):
+    access_token: str
+    token_type: str = "bearer"

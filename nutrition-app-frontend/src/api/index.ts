424331a38c152dd4c98f88fa7f183466c45e0afa
Gurdip Bath <bathgurdip1@gmail.com>
2025-07-22 19:41:06 +0100

    todo: added my own task to complete to push app forwards towards completion

--- nutrition-app-frontend/src/api/index.ts
+++ nutrition-app-frontend/src/api/index.ts
@@ -6,6 +6,7 @@ const api = axios.create({
   baseURL: 'http://localhost:8000/api',
 })
 
+// Attach Supabase token to every request
 api.interceptors.request.use(async (config) => {
   const {
     data: { session },
---
+++ nutrition-app-frontend/src/todo/todo.tsx
@@ -0,0 +1,4 @@
+// this keeps track of what I need to do so i don't forget 
+
+// Create a test route like GET /ping that uses Supabase auth
+
